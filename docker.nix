ab0e0a7762364e7d7a0ad436ee3243b1c0d11c61
Victor Freire <freirel.victor@gmail.com>
2021-02-14 22:37:07 -0300

    add db dir on docker

--- docker.nix
+++ docker.nix
@@ -6,10 +6,15 @@ dockerTools.buildImage {
   tag = "9a213bec1b81c0ed1a0bc7b854be4e678ae024d6";
   contents = [ moby-dick cacert ];
   
+  extraCommands = ''
+    mkdir -p db
+  '';
+
   config = {
     Cmd = [ "${moby-dick}/bin/moby-dick" ];
     Env = [
       "SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt"
+      "DB_PATH=db"
     ];
   };
 }
